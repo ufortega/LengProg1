ed837352d48560394e7a596272c41dcbb05d33e7
ufortega <39921099+ufortega@users.noreply.github.com>
2023-07-07 17:53:49 -0600

    Add files via upload
    Actividad 1 - Cálculo de edad en C++

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int edad;
+    std::cout << "Ingrese su edad: ";
+    std::cin >> edad;
+
+    if (edad >= 18) {
+        std::cout << "Usted es mayor de edad." << std::endl;
+    } else {
+        std::cout << "Usted es menor de edad." << std::endl;
+    }
+
+    return 0;
+}
+
